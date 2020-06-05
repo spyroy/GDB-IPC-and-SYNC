4a47f8ab415f43d83cbeb385b6ccbccad1f54b0e
matan <57196433+spyroy@users.noreply.github.com>
2020-06-05 12:26:55 +0300

    new

---
+++ SYNC/2.2.2.c
@@ -0,0 +1,212 @@
+#include <pthread.h> 
+#include <semaphore.h> 
+#include <stdio.h> 
+#include <time.h>
+#include <unistd.h>
+
+#define N 5 
+#define THINKING 2 
+#define HUNGRY 1 
+#define EATING 0 
+#define LEFT (phnum + 4) % N 
+#define RIGHT (phnum + 1) % N 
+#define HUNGER_LIM 10.0
+
+
+int state[N]; 
+int phil[N] = { 0, 1, 2, 3, 4 }; 
+clock_t forks[N];
+
+sem_t mutex; 
+sem_t S[N]; 
+
+
+/*
+this version of the code have low probability for starvation (if any)
+because we put timer for every philosopher and when philosopher wants to eat 
+he checks his neighbors timers, if the time is too big (in this code 5 seconds)
+than he doesnt eat and let his neighbors to eat,
+hence we have free starvation situation.
+
+eample of running the program:
+Philosopher 1 is thinking
+Philosopher 2 is thinking
+Philosopher 3 is thinking
+Philosopher 4 is thinking
+Philosopher 5 is thinking
+Philosopher 1 is Hungry
+Philosopher 1 takes fork 5 and 1
+Philosopher 1 is Eating
+Philosopher 2 is Hungry
+Philosopher 3 is Hungry
+Philosopher 3 takes fork 2 and 3
+Philosopher 3 is Eating
+Philosopher 4 is Hungry
+Philosopher 5 is Hungry
+Philosopher 1 putting fork 5 and 1 down
+Philosopher 1 is thinking
+Philosopher 3 putting fork 2 and 3 down
+Philosopher 3 is thinking
+Philosopher 4 takes fork 3 and 4
+Philosopher 4 is Eating
+Philosopher 1 is Hungry
+Philosopher 1 takes fork 5 and 1
+Philosopher 1 is Eating
+Philosopher 3 is Hungry
+Philosopher 4 putting fork 3 and 4 down
+Philosopher 4 is thinking
+Philosopher 1 putting fork 5 and 1 down
+Philosopher 1 is thinking
+Philosopher 2 takes fork 1 and 2
+Philosopher 2 is Eating
+Philosopher 4 is Hungry
+Philosopher 4 takes fork 3 and 4
+Philosopher 4 is Eating
+Philosopher 1 is Hungry
+Philosopher 2 putting fork 1 and 2 down
+Philosopher 2 is thinking
+Philosopher 4 putting fork 3 and 4 down
+Philosopher 4 is thinking
+Philosopher 5 takes fork 4 and 5
+Philosopher 5 is Eating
+Philosopher 2 is Hungry
+Philosopher 2 takes fork 1 and 2
+Philosopher 2 is Eating
+Philosopher 4 is Hungry
+Philosopher 5 putting fork 4 and 5 down
+Philosopher 5 is thinking
+Philosopher 2 putting fork 1 and 2 down
+Philosopher 2 is thinking
+Philosopher 3 takes fork 2 and 3
+Philosopher 3 is Eating
+Philosopher 5 is Hungry
+Philosopher 5 takes fork 4 and 5
+Philosopher 5 is Eating
+Philosopher 2 is Hungry
+Philosopher 3 putting fork 2 and 3 down
+Philosopher 3 is thinking
+Philosopher 5 putting fork 4 and 5 down
+Philosopher 5 is thinking
+Philosopher 1 takes fork 5 and 1
+Philosopher 1 is Eating
+	.
+	.	
+	.
+*/
+
+
+void test(int phnum) 
+{ 	
+	if (state[phnum] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING 
+		&& ((clock() - forks[LEFT])/CLOCKS_PER_SEC < 5.0)
+		&& ((clock() - forks[RIGHT])/CLOCKS_PER_SEC < 5.0)) {
+ 
+		// state that eating 
+		state[phnum] = EATING; 
+	
+		sleep(2);
+		
+		//zero time
+		forks[phnum] = clock();
+
+		printf("Philosopher %d takes fork %d and %d\n", phnum + 1, LEFT + 1, phnum + 1); 
+
+		printf("Philosopher %d is Eating\n", phnum + 1); 
+
+
+		// sem_post(&S[phnum]) has no effect 
+		// during takefork 
+		// used to wake up hungry philosophers 
+		// during putfork 
+		sem_post(&S[phnum]); 
+	} 
+} 
+
+// take up chopsticks 
+void take_fork(int phnum) 
+{ 
+
+	sem_wait(&mutex); 
+
+	// state that hungry 
+	state[phnum] = HUNGRY; 
+
+	printf("Philosopher %d is Hungry\n", phnum + 1); 
+
+	// eat if neighbours are not eating 
+	test(phnum); 
+
+	sem_post(&mutex); 
+
+	// if unable to eat wait to be signalled 
+	sem_wait(&S[phnum]); 
+
+	sleep(1); 
+} 
+
+// put down chopsticks 
+void put_fork(int phnum) 
+{ 
+
+	sem_wait(&mutex); 
+
+	// state that thinking 
+	state[phnum] = THINKING; 
+
+	printf("Philosopher %d putting fork %d and %d down\n", phnum + 1, LEFT + 1, phnum + 1); 
+	printf("Philosopher %d is thinking\n", phnum + 1); 
+
+	test(LEFT);
+	test(RIGHT); 
+
+	sem_post(&mutex); 
+} 
+
+void* philospher(void* num) 
+{ 	
+
+	int* i = num;
+ 
+	// initialize time 
+	forks [*i] = clock();
+
+	while (1) { 
+
+		sleep(1); 
+
+		take_fork(*i); 
+
+		sleep(0); 
+
+		put_fork(*i); 
+	} 
+} 
+
+int main() 
+{ 
+
+	int i; 
+	pthread_t thread_id[N]; 
+
+	// initialize the mutexes 
+	sem_init(&mutex, 0, 1); 
+
+	for (i = 0; i < N; i++) 
+
+		sem_init(&S[i], 0, 0); 
+
+
+	for (i = 0; i < N; i++) { 
+
+		// create philosopher processes 
+		pthread_create(&thread_id[i], NULL, philospher, &phil[i]); 
+		state[i] = THINKING;
+
+		printf("Philosopher %d is thinking\n", i + 1); 
+	} 
+
+ 
+	for (i = 0; i < N; i++) 
+
+		pthread_join(thread_id[i], NULL); 
+} 
